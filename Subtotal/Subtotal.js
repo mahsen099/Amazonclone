9475a93da0665c590b68f5d9ed1e3a6593fb455d
mahsen kemal <mahsen099@gmail.com>
2021-02-25 20:50:10 -0600

    Update Subtotal.js

--- Subtotal/Subtotal.js
+++ Subtotal/Subtotal.js
@@ -8,7 +8,7 @@ function Subtotal() {
         renderText={(value) => (
           <div>
             <p>
-              Subtotal (01 items): <strong>0</strong>
+              Subtotal (0 items): <strong>0</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
